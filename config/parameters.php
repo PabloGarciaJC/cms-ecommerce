3bfa45f08f0bd18aeff4ea420c193e6b06704387
PabloGarciaJC <pablogarcia-jc@hotmail.com>
2021-08-19 08:11:49 +0200

    Inicio del Proyecto

--- config/parameters.php
+++ config/parameters.php
@@ -1,6 +1,10 @@
 <?php
 
-define("base_url", "http://localhost/Portafolios/pablogarciajc-ecommerce/");
+//hosting Heroku
+ define("base_url", "https://pablogarciajc-ecommerce.herokuapp.com/");
+
+//localhost
+//define("base_url", "http://localhost/Portafolios/pablogarciajc-ecommerce/");
+
 define("controller_default","HomeController");
 define("action_default", "index");
-//define("action_default","default");
