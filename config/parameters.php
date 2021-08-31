5ac113a8f7cc017069bb20c98f49c94eb7ea6dad
PablogarciaJC <pablogarcia-jc@hotmail.com>
2021-08-31 18:11:10 +0200

    actualizacion

--- config/parameters.php
+++ config/parameters.php
@@ -1,10 +1,10 @@
 <?php
 
 //hosting Heroku
- define("base_url", "https://pablogarciajc-ecommerce.herokuapp.com/");
+//  define("base_url", "https://pablogarciajc-ecommerce.herokuapp.com/");
 
 //localhost
-//define("base_url", "http://localhost/Portafolios/pablogarciajc-ecommerce/");
+define("base_url", "http://localhost/Portafolios/pablogarciajc-ecommerce/");
 
 define("controller_default","HomeController");
 define("action_default", "index");
