244a304b06216a92a585e6ab0af4e7248a200ec4
PabloGarciaJC <64238590+PabloGarciaJC@users.noreply.github.com>
2021-09-18 00:06:13 +0200

    Update parameters.php

--- config/parameters.php
+++ config/parameters.php
@@ -4,7 +4,7 @@
 //  define("base_url", "https://pablogarciajc-ecommerce.herokuapp.com/");
 
 //localhost
-define("base_url", "http://localhost/Portafolios/pablogarciajc-ecommerce/");
+define("base_url", "http://localhost/Portafolios/pablogarciajc_ecommerce/");
 
 define("controller_default","HomeController");
 define("action_default", "index");
