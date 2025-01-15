ee1ec9ffac1766c10445cc04579e0a1d6981ddc0
PabloGarciaJC <pablogarciiajc@gmail.com>
2025-01-15 11:13:53 +0100

    tes

--- controllers/FavoritoController.php
+++ controllers/FavoritoController.php
@@ -89,14 +89,14 @@ class FavoritoController
         $favorito->setGrupoId($grupoId);
         $existe = $favorito->existe();
 
-        // if (!$existe) {
-        //     echo json_encode([
-        //         'success' => true,
-        //         'favorito' => true,
-        //         'message' => TEXT_PRODUCT_ALREADY_FAVORITE
-        //     ]);
-        //     return;
-        // }
+        if (!$existe) {
+            echo json_encode([
+                'success' => true,
+                'favorito' => true,
+                'message' => TEXT_PRODUCT_ALREADY_FAVORITE
+            ]);
+            return;
+        }
 
         var_dump($existe);
         // $resultado = $favorito->eliminarFronted();
